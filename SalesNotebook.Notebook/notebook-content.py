6ea2ec21f698cbbf1bb85f74ec987ca933eee927
SandeepTiwari-mct <sandeep.tiwari20033@outlook.com>
2026-04-30 08:10:56 +0530

    Committing 1 item from workspace c758d7f7-d06a-4903-b9d5-98c1b7f4881b

---
+++ SalesNotebook.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "9cd4be06-1a9a-40ab-afcd-93141b73db42",
+# META       "default_lakehouse_name": "LKH1",
+# META       "default_lakehouse_workspace_id": "c758d7f7-d06a-4903-b9d5-98c1b7f4881b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "9cd4be06-1a9a-40ab-afcd-93141b73db42"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
